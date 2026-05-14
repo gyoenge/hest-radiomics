c4ef6871125040bd89512b694aeda43267ff96fa
gyoenge <gyoeng94@gmail.com>
2026-05-15 01:05:12 +0900

    fix: fix _download_hest

--- src/hestradiomics/_download_hest.py
+++ src/hestradiomics/_download_hest.py
@@ -11,7 +11,7 @@ import scanpy as sc
 from dotenv import load_dotenv
 from huggingface_hub import HfApi, login, snapshot_download
 
-from hestradiomics._utils import ensure_dir, setup_logger
+from hestradiomics._utils import ensure_dir 
 from hestradiomics.config import (
     DOWNLOAD_ROOT, 
     DOWNLOAD_ONCOTREE, 
